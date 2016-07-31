65d8c8678b0445968306da343642c297eba1babe
mathasaiamar <amarhussey@gmail.com>
2016-07-31 14:31:30 +0530

    Update sqrtcbrt.java

--- sqrtcbrt.java
+++ sqrtcbrt.java
@@ -10,19 +10,11 @@ public class sqrtcbrt {
         System.out.print("enter a number:");
 		s = new Scanner(System.in);
         n = s.nextInt();
-        System.out.println("enter the square root of n: "+sqrt(n));
-        System.out.println("enter the qube root of n: "+cbrt(n));
-	}
+        System.out.println("enter the square root of n: "+Math.sqrt(n));
+        System.out.println("enter the qube root of n: "+Math.cbrt(n));
+	
 
-	private static int cbrt(int n) {
-		// TODO Auto-generated method stub
-		return n*n*n;
-	}
-
-	private static int sqrt(int n) {
-		// TODO Auto-generated method stub
-		return n*n;
-		
+	
 	}
 
 }
